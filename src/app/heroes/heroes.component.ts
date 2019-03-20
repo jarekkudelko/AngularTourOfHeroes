43b61d3f1fb84142ffc92e7802cceefd9959a4b9
Jarosław Kudelko <jkudelko@prointegra.com.pl>
2019-03-20 10:13:09 +0100

    Displaying a list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from '..//hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,13 +9,20 @@ import {Hero} from '..//hero';
 })
 export class HeroesComponent implements OnInit {
 
+  constructor() { }
+
   hero: Hero = {
     id : 1,
     name : 'Windstorm'
   };
 
 
-  constructor() { }
+  selectedHero: Hero;
+
+  heroes = HEROES;
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
 
   ngOnInit() {
   }
